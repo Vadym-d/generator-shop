c2eabffcbd7c8d4bf67a01016d0ab0c1b92b2195
rewqla <desares526@gmail.com>
2023-11-15 20:34:35 +0200

    Add a generic repo

---
+++ Infrastructure/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Interfaces
+{
+    interface IGenericRepository<TEntity> where TEntity : class
+    {
+        TEntity FindById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Add(TEntity item);
+        void Delete(TEntity item);
+        void Update(TEntity item);
+    }
+}
