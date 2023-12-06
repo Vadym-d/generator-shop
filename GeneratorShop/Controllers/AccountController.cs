2a449031ed98b1c57eaf456d2ca24b800bd544d4
rewqla <desares526@gmail.com>
2023-12-06 14:22:46 +0200

    Add all changes from the master

--- GeneratorShop/Controllers/AccountController.cs
+++ GeneratorShop/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Entities;
 using GeneratorShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -44,12 +45,12 @@ namespace GeneratorShop.Controllers
                     }
                     else
                     {
-                        ModelState.AddModelError("Password", "Invalid login attempt. Please check your email and password.");
+                        ModelState.AddModelError("Password", "Не вдалось увійти. Періврте свій пароль і пошту");
                     }
                 }
                 else
                 {
-                    ModelState.AddModelError("Password", "User not found. Please check your email and try again.");
+                    ModelState.AddModelError("Password", "Не знайдено такого користувача");
                 }
             }
 
@@ -86,18 +87,102 @@ namespace GeneratorShop.Controllers
                     return Redirect(model.ReturnUrl);
                 }
 
-                ModelState.AddModelError("", "Oooops some errors");
+                ModelState.AddModelError("", "Ой щось не ладне коїться");
             }
 
             return View(model);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Logout(string returnUrl = null)
+        public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
 
-            return Redirect(returnUrl);
+            return Redirect("/");
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> UserManagment()
+        {
+            DbUser user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Phone = user.PhoneNumber,
+                Email = user.Email,
+            };
+
+            return View(model);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangeProfile(EditUserViewModel model)
+        {
+            ModelState.Remove("NewPassword");
+            ModelState.Remove("Password");
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+                user.Email = model.Email;
+                user.PhoneNumber = model.Phone;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return View("UserManagment", model); ;
+                }
+                else
+                {
+                    ModelState.AddModelError("Phone", "Не вдалось оновити профіль. Спробуйте ще раз");
+                }
+            }
+
+            return View("UserManagment", model);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(EditUserViewModel model)
+        {
+            ModelState.Remove("Email");
+            ModelState.Remove("Phone");
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, model.Password);
+
+                if (passwordCheck)
+                {
+                    var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.Password, model.NewPassword);
+
+                    if (changePasswordResult.Succeeded)
+                    {
+                        return RedirectToAction("/");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Не вдалось змінити пароль. Спробуйте ще раз");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Password", "Не вірний старий пароль");
+                }
+            }
+
+            return View("UserManagment", model);
         }
     }
 }
